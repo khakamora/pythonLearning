19ff4d992cf4b2fde4bd0b692a6c452a5646be57
khakamora <yuriy.mamchurovskyy@gmail.com>
2018-10-03 00:17:28 +0300

    final 3rd task, start to work under 4 task

---
+++ lab3/lab3/Task4.py
@@ -0,0 +1,7 @@
+'''
+Created on 2 жовт. 2018 р.
+
+@author: F
+'''
+
+my_string = "Life is a trap. We are under attack, please help us. We are waiting for you!!!"
--- lab3/lab3/task2_2.py
+++ lab3/lab3/task2_2.py
@@ -3,3 +3,22 @@ Created on 29 вер. 2018 р.
 
 @author: F
 '''
+import re
+
+my_string = "Ф;И;О;Возраст;Категория;_Иванов;Иван;Иванович;23 года;Студент 3 курса;_Петров;Семен;Игоревич;22 года;Студент 2 курса"
+
+spl_list = re.split('_',my_string)
+
+str1 = spl_list[0]
+header1 = str1.replace(";","", 2)
+
+str2 = spl_list[1]
+row1 = str2.replace(";"," ")
+
+str3 = spl_list[2]
+row2 = str3.replace(";"," ")
+
+print('{:<24}{:<14}{}'.format(header1[0:3], header1[4:11], header1[12:21]))
+print('{:<24}{:<14}{}'.format(row1[0:20], row1[21:28], row1[29:44]))
+print('{:<23}{:<14}{}'.format(row2[0:21], row2[21:28], row2[29:45]))
+
